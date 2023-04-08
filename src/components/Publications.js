82985059e4085802163feaa6116ef62500e049c2
nitesh047 <niteshsaini5112001@gmail.com>
2023-04-08 21:44:14 +0530

    publication page done

--- src/components/Publications.js
+++ src/components/Publications.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './index.css';
-import { Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { BsArrowRight } from 'react-icons/bs';
 function Publications() {
   return (
     <>
@@ -21,38 +23,49 @@ function Publications() {
 
             </div>
             <div className='col-md-10 '>
+                <div className='container mainPubHeading'>
+                <Typography>
+                h-index: 10, i-10 index: 15, Total Citations: 554
+               
+                </Typography>
+                <Button style={{background:'white',borderColor:'#3cbcba',color:'#3cbcba'}} variant='outlined' href="https://scholar.google.com/citations?user=R2-cQjAAAAAJ&hl=en" target='blank'>
+                       Google Scholar Website
+                </Button>
+
+                </div>
+                <br></br>
                 <Typography variant='h4'>
                 2022
                 </Typography>
                 <br></br>
                 <Typography className='publicationName' >
-                    1.  “Quantification of dead lithium on graphite anode under fast charging conditions,” K. S. N. Vikrant, E. McShane, A. M. Colclasure, B. D. McCloskey,
-S. Allu, Journal of The Electrochemical Society, 169, 040520, (2022). IF:
-4.316
+                    1.  “Quantification of dead lithium on graphite anode under fast charging conditions,” <span style={{fontWeight:'700'}}>K. S. N. Vikrant</span>, E. McShane, A. M. Colclasure, B. D. McCloskey,
+S. Allu, Journal of The Electrochemical Society, 169, 040520, (2022). <span style={{color:'#ef405e'}}>IF:
+4.316</span>
                 </Typography><br></br>
                 <Typography className='publicationName'>
                     2. “Grain boundary metal–insulator transitions in polycrystalline LiCoO2,” L.
-D. Robinson, J. Lund, K. S. N. Vikrant, R. E. García, Journal of Power Sources,
-547, 231918, (2022). IF: 9.719
+D. Robinson, J. Lund, <span style={{fontWeight:'700'}}>K. S. N. Vikrant</span>, R. E. García, Journal of Power Sources,
+547, 231918, (2022). <span style={{color:'#ef405e'}}>IF: 9.719</span>
 
                 </Typography>
                 <br></br>
                 <Typography className='publicationName'>
-                  3. “Atomistic modeling of LiF microstructure ionic conductivity and its influence on nucleation and plating,” L. Alzate-Vargas, K. S. N. Vikrant, S. Allu,
+                  3. “Atomistic modeling of LiF microstructure ionic conductivity and its influence on nucleation and plating,” L. Alzate-Vargas, <span style={{fontWeight:'700'}}>K. S. N. Vikrant</span>, S. Allu,
 J-L. Fattebert, Physical Review Materials, 6, 095402, (2022).
 
                 </Typography>
                 <br></br>
                 <Typography className='publicationName'>
                   4. “Interfacial and volumetric melting gegimes of Sn nanoparticles,” L. D.
-Robinson, K. S. N. Vikrant, J. E. Blendell, C.A. Handwerker, R. E. García,
-Acta Materialia, 235, 118084, (2022). IF: 9.209
+Robinson, <span style={{fontWeight:'700'}}>K. S. N. Vikrant</span>, J. E. Blendell, C.A. Handwerker, R. E. García,
+Acta Materialia, 235, 118084, (2022). <span style={{color:'#ef405e'}}>IF: 9.209</span>
 
                 </Typography>
                 <br></br>
                 <Typography className='publicationName'>
-               5. “liionpack: A Python package for simulating packs of batteries with PyBaMM,” T. Tranter, R. Timms, V. Sulzer, F. Planella, G. Wiggins, K. S. N.
-Vikrant, P. Agarwal, S. Chopra, S. Allu, P. Shearing, D. Bret, Journal of The
+               5. “liionpack: A Python package for simulating packs of batteries with PyBaMM,” T. Tranter, R. Timms, V. Sulzer, F. Planella, G. Wiggins,<b> K. S. N.
+Vikrant</b>, P. Agarwal, S. Chopra, S. Allu, P. Shearing, D. Bret, Journal of The
 Electrochemical Society, 169, 040520, (2022).
                 </Typography>
                 <br></br><br></br>
@@ -62,27 +75,27 @@ Electrochemical Society, 169, 040520, (2022).
                 <Typography className='publicationName'>
               1.  “Core-shell metallic alloy nanopillars-in-dielectric hybrid metamaterials
 with magneto-plasmonic coupling,” J. Huang, X. L. Phuah, L. M. McClintock, P. Padmanabhan, K.S.N. Vikrant, et.al, Materials Today, 51, 39–47,
-(2021). IF: 26.94
+(2021). <span style={{color:'#ef405e'}}>IF: 26.94</span>
 
                 </Typography><br></br>
                 <Typography className='publicationName'>
-             2. “Modeling of lithium nucleation and plating kinetics under fast charge conditions,” K. S. N. Vikrant, S. Allu, Journal of The Electrochemical Society,
-168, 020536, (2021). IF: 4.316
+             2. “Modeling of lithium nucleation and plating kinetics under fast charge conditions,” <span style={{fontWeight:'700'}}>K. S. N. Vikrant</span>, S. Allu, Journal of The Electrochemical Society,
+168, 020536, (2021). <span style={{color:'#ef405e'}}>IF: 4.316</span>
                 </Typography><br></br>
                 <Typography className='publicationName'>
-             3. “Modeling of flash sintering of ionic ceramics,” K. S. N. Vikrant, X. L. Phuah,
+             3. “Modeling of flash sintering of ionic ceramics,” <span style={{fontWeight:'700'}}>K. S. N. Vikrant</span>, X. L. Phuah,
 J. Lund, H. Wang, C. S. Hellberg, N. Bernstein, W. Rheinheimer, C. M. Bishop,
-H. Wang, R. E. García, MRS Bulletin, 46, 67–75, (2021). IF: 5.199
+H. Wang, R. E. García, MRS Bulletin, 46, 67–75, (2021). <span style={{color:'#ef405e'}}>IF: 5.199</span>
                 </Typography><br></br>
                 <Typography className='publicationName'>
-             4.  “Thermodynamically consistent variational principles for charged interfaces,” J. Lund, K. S. N. Vikrant, W. Rheinheimer, C. M. Bishop, R. E. García,
-Acta Materialia, 205, 116525, (2021). IF: 9.209
+             4.  “Thermodynamically consistent variational principles for charged interfaces,” J. Lund, <span style={{fontWeight:'700'}}>K. S. N. Vikrant</span>, W. Rheinheimer, C. M. Bishop, R. E. García,
+Acta Materialia, 205, 116525, (2021). <span style={{color:'#ef405e'}}>IF: 9.209</span>
                 </Typography><br></br>
                 <Typography className='publicationName'>
            5. “Grain boundary stabilized zirconium monoxide under DC electric field,” H.
-Charalambous, S. K. Jha, K. S. N. Vikrant, R. E. García, X. L. Phuah, H. Wang,
+Charalambous, S. K. Jha, <span style={{fontWeight:'700'}}>K. S. N. Vikrant</span>, R. E. García, X. L. Phuah, H. Wang,
 H. Wang, A. Mukherjee, J. S. Okasinski, T. Tsakalakos, Acta Materialia, 204,
-114130, (2021). IF: 5.611
+114130, (2021). <span style={{color:'#ef405e'}}>IF: 5.611</span>
 
                 </Typography><br></br>
                 <br></br>
@@ -92,31 +105,31 @@ H. Wang, A. Mukherjee, J. S. Okasinski, T. Tsakalakos, Acta Materialia, 204,
                 <Typography className='publicationName'>
           1. . “Electrochemically driven abnormal grain growth in ionic ceramicss,” K.
 S. N. Vikrant, W. Rheinheimer, H. Sternlicht, M. Bäurer, R. E. García, Acta
-Materialia, 200, 727–734, (2020). IF: 9.209
+Materialia, 200, 727–734, (2020). <span style={{color:'#ef405e'}}>IF: 9.209</span>
 
                 </Typography><br></br>
                 <Typography className='publicationName'>
           2. “Ultrahigh temperature in situ transmission electron microscopy based
-bicrystal coble creep in zirconia I: Nanowire growth and interfacial diffusivity,” K. S. N. Vikrant, R. L. Grosso, L. Feng, E. N. S. Muccillo, D. N. F.
+bicrystal coble creep in zirconia I: Nanowire growth and interfacial diffusivity,” <span style={{fontWeight:'700'}}>K. S. N. Vikrant</span>, R. L. Grosso, L. Feng, E. N. S. Muccillo, D. N. F.
 Muche, G. S. Jawaharram, C. M. Barr, A. M. Monterrosa, R. H. R. Castro, R.
 E. García, K. Hattar, S. J. Dillon, Acta Materialia, 199, 530–541, (2020).
-IF: 9.209
+<span style={{color:'#ef405e'}}>IF: 9.209</span>
                 </Typography><br></br>
                 <Typography className='publicationName'>
         3.  “Ultrahigh temperature in situ transmission electron microscopy based
-bicrystal coble creep in zirconia II: Nanowire growth and interfacial diffusivity,” R. L. Grosso, K. S. N. Vikrant, L. Feng, E. N. S. Muccillo, D. N. F.
+bicrystal coble creep in zirconia II: Nanowire growth and interfacial diffusivity,” R. L. Grosso, <span style={{fontWeight:'700'}}>K. S. N. Vikrant</span>, L. Feng, E. N. S. Muccillo, D. N. F.
 Muche, G. S. Jawaharram, C. M. Barr, A. M. Monterrosa, R. H. R. Castro, R.
 E. García, K. Hattar, S. J. Dillon, Acta Materialia, 200, 1008–1021, (2020).
-IF: 9.209 
+<span style={{color:'#ef405e'}}>IF: 9.209</span> 
                 </Typography><br></br>
                 <Typography className='publicationName'>
-       4. “Electrochemical-Drag effect in charged grain boundary motion,” K. S. N.
-Vikrant, W. Rheinheimer, R. E. García, Nature Partner Journal: Computational Materials, 6, 165, (2020). IF: 10.76
+       4. “Electrochemical-Drag effect in charged grain boundary motion,”<b> K. S. N.
+Vikrant</b>, W. Rheinheimer, R. E. García, Nature Partner Journal: Computational Materials, 6, 165, (2020). <span style={{color:'#ef405e'}}>IF: 10.76</span>
                 </Typography><br></br>
                 <Typography className='publicationName'>
-     5. “Flash sintering incubation kinetics,” K. S. N. Vikrant, H. Wang, A. Jana,
+     5. “Flash sintering incubation kinetics,” <span style={{fontWeight:'700'}}>K. S. N. Vikrant</span>, H. Wang, A. Jana,
 H. Wang, R. E. García, Nature Partner Journal: Computational Materials, 6,
-98, (2020). IF: 10.76
+98, (2020). <span style={{color:'#ef405e'}}>IF: 10.76</span>
 
                 </Typography><br></br>
                
@@ -126,28 +139,28 @@ H. Wang, R. E. García, Nature Partner Journal: Computational Materials, 6,
                 </Typography><br></br>
                 <Typography className='publicationName'>
     1. “Electrochemomechanics of lithium dendrite growth,” A. Jana, S. I. Woo,
-K. S. N. Vikrant, R. E. García, Energy & Environmental Science, 12, 3595–
-3607, (2019). IF: 38.53
+<span style={{fontWeight:'700'}}>K. S. N. Vikrant</span>, R. E. García, Energy & Environmental Science, 12, 3595–
+3607, (2019). <span style={{color:'#ef405e'}}>IF: 38.53</span>
 
                 </Typography><br></br>
                 <Typography className='publicationName'>
-   2. . “Charged grain boundary transitions in ionic ceramics for energy applications,” K. S. N. Vikrant, R. E. García, Nature Partner Journal: Computational
-Materials, 5, 24, (2019). IF: 9.651
+   2. . “Charged grain boundary transitions in ionic ceramics for energy applications,” <span style={{fontWeight:'700'}}>K. S. N. Vikrant</span>, R. E. García, Nature Partner Journal: Computational
+Materials, 5, 24, (2019). <span style={{color:'#ef405e'}}>IF: 9.651</span>
                 </Typography><br></br>
                 <br></br>
                 <Typography variant='h4'>
                 2018
                 </Typography><br></br>
                 <Typography className='publicationName'>
-  1.  “Charged Interfaces: Electrochemical and Mechanical Effects,” K. S. N.
-Vikrant, W. C. Chueh, R. E. García, Energy & Environmental Science, 11,
-1993–2000, (2018). IF: 38.53
+  1.  “Charged Interfaces: Electrochemical and Mechanical Effects,”<b> K. S. N.
+Vikrant</b>, W. C. Chueh, R. E. García, Energy & Environmental Science, 11,
+1993–2000, (2018). <span style={{color:'#ef405e'}}>IF: 38.53</span>
 
                 </Typography><br></br>
                 <Typography className='publicationName'>
-  2. “High temperature deformability of ductile flash-sintered ceramics via insitu compression”, J. Cho, Q. Li, H. Wang, Z. Fan, J. Li, S. Xue, K. S. N.
-Vikrant, H. Wang, T. B. Holland, A. K. Mukherjee, R. E. García, X. Zhang,
-Nature Communications, 9, 2063, (2018). IF: 17.69
+  2. “High temperature deformability of ductile flash-sintered ceramics via insitu compression”, J. Cho, Q. Li, H. Wang, Z. Fan, J. Li, S. Xue,<b> K. S. N.
+Vikrant</b>, H. Wang, T. B. Holland, A. K. Mukherjee, R. E. García, X. Zhang,
+Nature Communications, 9, 2063, (2018). <span style={{color:'#ef405e'}}>IF: 17.69</span>
 
 
                 </Typography><br></br>
@@ -157,9 +170,9 @@ Nature Communications, 9, 2063, (2018). IF: 17.69
                 </Typography><br></br>
                 <Typography className='publicationName'>
  1. “Development and evaluation of SUS 304H—IN 617 welds for advanced
-ultra supercritical boiler applications,” A. H. V. Pavan, K. S. N. Vikrant, R.
+ultra supercritical boiler applications,” A. H. V. Pavan, <span style={{fontWeight:'700'}}>K. S. N. Vikrant</span>, R.
 Ravibharath, K. Singh, Materials Science and Engineering A, 642, 32–41,
-(2015). IF: 4.081
+(2015). <span style={{color:'#ef405e'}}>IF: 4.081</span>
 
 
                 </Typography><br></br>
@@ -169,16 +182,16 @@ Ravibharath, K. Singh, Materials Science and Engineering A, 642, 32–41,
                 </Typography><br></br>
                 <Typography className='publicationName'>
  1. “Estimation of residual life of boiler tubes using steam side oxide scale
-thickness,” K. S. N. Vikrant, G. V. Ramareddy, A. H. V. Pavan, K. Singh,
+thickness,” <span style={{fontWeight:'700'}}>K. S. N. Vikrant</span>, G. V. Ramareddy, A. H. V. Pavan, K. Singh,
 International Journal of Pressure Vessels and Piping, 104, 69–75, (2013).
-IF: 2.075
+<span style={{color:'#ef405e'}}>IF: 2.075</span>
 
 
 
                 </Typography><br></br>
                 <Typography className='publicationName'>
  2. “Metallurgical analysis of SA-106Gr. B pipe failure during hot bending,”
-A. H. V.Pavan, K. S. N. Vikrant, G. Vimalan, K. Singh, Case Studies in Engineering Failure Analysis, 1, 120–130, (2013). IF: 1.891
+A. H. V.Pavan, <span style={{fontWeight:'700'}}>K. S. N. Vikrant</span>, G. Vimalan, K. Singh, Case Studies in Engineering Failure Analysis, 1, 120–130, (2013). <span style={{color:'#ef405e'}}>IF: 1.891</span>
 
 
 
@@ -186,23 +199,23 @@ A. H. V.Pavan, K. S. N. Vikrant, G. Vimalan, K. Singh, Case Studies in Engineeri
                 <Typography className='publicationName'>
  3. “Root cause analysis of bowl-mill pinion shaft failures,” A. H. V. Pavan, K.
 S. N. Vikrant, M. Swamy, G. Jayaraman, Case Studies in Engineering Failure
-Analysis, 1, 103–109, (2013). IF: 1.891
+Analysis, 1, 103–109, (2013). <span style={{color:'#ef405e'}}>IF: 1.891</span>
                 </Typography><br></br>
                 <Typography className='publicationName'>
-4. . “Creep modeling of P91 steel for high temperature power plant applications,” S. Nandi, K. S. N. Vikrant, A. H. V. Pavan, K. Singh, Procedia Engineering, 55, 751–755, (2013). IF: 1.04
-                </Typography><br></br>
+4. . “Creep modeling of P91 steel for high temperature power plant applications,” S. Nandi, <span style={{fontWeight:'700'}}>K. S. N. Vikrant</span>, A. H. V. Pavan, K. Singh, Procedia Engineering, 55, 751–755, (2013). <span style={{color:'#ef405e'}}>IF: 1.04
+</span>                </Typography><br></br>
                 <br></br>
                 <Typography variant='h4'>
                 2012
                 </Typography><br></br>
                 <Typography className='publicationName'>
 1. “Characterization of fireside corroded sections of boiler tubes,” A. H. V.
-Pavan, K. S. N. Vikrant, S. Nandi, K. Singh, Materials Science Forum, 710,
-701–706, (2012). IF: 0.35
-                </Typography><br></br>
+Pavan, <span style={{fontWeight:'700'}}>K. S. N. Vikrant</span>, S. Nandi, K. Singh, Materials Science Forum, 710,
+701–706, (2012). <span style={{color:'#ef405e'}}>IF: 0.35
+</span>                </Typography><br></br>
                 <Typography className='publicationName'>
 2. “Theoretical estimation of thickness variation in thermal barrier coatings
-by using pulse phase thermography,” M. M. Kumar, K. S. N. Vikrant, M.
+by using pulse phase thermography,” M. M. Kumar, <span style={{fontWeight:'700'}}>K. S. N. Vikrant</span>, M.
 Swamy, M. S. Rawat, R. Markandeya QIRT, (2012).
 
                 </Typography><br></br>
