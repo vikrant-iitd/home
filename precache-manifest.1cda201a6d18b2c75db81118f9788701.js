e5c9c97caac8347d9d33002b31cc5e646a33be8d
manojkgorle <kfghjkl091@gmail.com>
2024-01-08 22:37:46 +0530

    Updates

---
+++ precache-manifest.1cda201a6d18b2c75db81118f9788701.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8460fc943a26df12596",
+    "url": "/vikrant-work-website/website/static/css/main.c8666fa3.chunk.css"
+  },
+  {
+    "revision": "e8460fc943a26df12596",
+    "url": "/vikrant-work-website/website/static/js/main.e8460fc9.chunk.js"
+  },
+  {
+    "revision": "fdeb34be957d0625724f",
+    "url": "/vikrant-work-website/website/static/js/1.fdeb34be.chunk.js"
+  },
+  {
+    "revision": "cf8c1abbc307bda1d528",
+    "url": "/vikrant-work-website/website/static/css/2.f7530b78.chunk.css"
+  },
+  {
+    "revision": "cf8c1abbc307bda1d528",
+    "url": "/vikrant-work-website/website/static/js/2.cf8c1abb.chunk.js"
+  },
+  {
+    "revision": "f847e71c8d2d65b6e0c7",
+    "url": "/vikrant-work-website/website/static/js/runtime~main.f847e71c.js"
+  },
+  {
+    "revision": "5617d94994e7c8e2ec7e697987e92b9f",
+    "url": "/vikrant-work-website/website/static/media/unnamed.5617d949.jpg"
+  },
+  {
+    "revision": "23674a89105f4543611c4df558041999",
+    "url": "/vikrant-work-website/website/index.html"
+  }
+];
